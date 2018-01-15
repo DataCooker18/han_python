f14e1e6a1aacf570c88ff39c1b73f90252534d54
DataCooker18 <35443162+DataCooker18@users.noreply.github.com>
2018-01-15 15:30:55 +0900

    1-5
    1회 ~ 5회

---
+++ 5회/ex.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import hot_sauce
+
+hot_sauce.hot_sum(1, 2, 3)
+hot_sauce.hot_mul(1, 2, 3)
+hot_sauce.hot_avg(1, 2, 3)
---
+++ 5회/hot_sauce.py
@@ -0,0 +1,13 @@
+def hot_sum (a, b, c):
+    total_sum = a + b + c
+    return total_sum
+    
+    
+def hot_mul(a, b, c):
+    total_mul = a * b * c
+    return total_mul
+
+def hot_avg(a, b, c):
+    total_sum = a + b + c
+    return total_sum/3
+
